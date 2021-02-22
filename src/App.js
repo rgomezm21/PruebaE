2754006a4103ee6a26a4047d6839eeb2751a8f02
rgomezm21 <rauleduard132@gmail.com>
2021-02-22 03:53:47 -0600

    V1.0.1

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MaterialTable from 'material-table';
-// import {Modal, TextField, Button} from '@material-ui/core';
-// import {makeStyles} from '@material-ui/core/styles';
+import {Modal, TextField, Button, Container} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
+import {Alert} from 'reactstrap';
 
 const columnas = [
   {
@@ -26,19 +27,110 @@ const columnas = [
 ]
 
 const url = "https://jsonplaceholder.typicode.com/users";
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)'
+  },
+  iconos:{
+    cursor: 'pointer'
+  }, 
+  inputMaterial:{
+    width: '100%'
+  }
+}));
+
 function App() {
-
+  const styles = useStyles();
   const [data, setData]=useState([]);
+  const [moEditar, setMoEditar]= useState(false);
+  const [datosEmpleado, setDatosEmpleado]=useState({
+    name: "",
+    username: "",
+    email: "",
+    phone: "",
+    company:""
+  })
 
+  const handleChange=e=>{
+    const {name, value}=e.target;
+    setDatosEmpleado(prevState=>({
+      ...prevState,
+      [name]: value
+    }));
+  }
   const Get=async()=>{
     await axios.get(url).then(response=>{
       setData(response.data);
     })
   }
+
+  const peticionPut=async()=>{
+    await axios.put(url+"/"+datosEmpleado.id, datosEmpleado)
+    .then(response=>{
+      let dataNueva= data;
+      dataNueva.map(name=>{
+        if(name.id===datosEmpleado.id){
+          name.name=datosEmpleado.name;
+          name.username=datosEmpleado.username;
+          name.email=datosEmpleado.email;
+          name.phone=datosEmpleado.phone;
+          name.company=datosEmpleado.company;
+        }
+      });
+      setData(dataNueva);
+      abrirCerrarMoEditar();
+    }).catch(error=>{
+      console.log(error);
+    })
+  }
+
+const usuarioSeleccionado = (name, caso)=>{
+  setDatosEmpleado(name);
+  (caso==="Editar")&&abrirCerrarMoEditar()
+}
+
+  const abrirCerrarMoEditar=()=>{
+    setMoEditar(!moEditar);
+  }
+
   useEffect(()=>{
    Get();
-  },[])
+  },[]);
+
+  const bodyEditar=(
+    <div className={styles.modal}>
+      <h3>Editar Usuario</h3>
+      <TextField className={styles.inputMaterial} label="Nombre" name="name" onChange={handleChange} value={datosEmpleado&&datosEmpleado.name}/>
+      <br />
+      <TextField className={styles.inputMaterial} label="Usuario" name="username" onChange={handleChange} value={datosEmpleado&&datosEmpleado.username}/>          
+      <br />
+      <TextField disabled className={styles.inputMaterial} label="Email" name="email" onChange={handleChange} value={datosEmpleado&&datosEmpleado.email}/>
+      <br />
+      <TextField className={styles.inputMaterial} label="Telefono" name="phone" onChange={handleChange} value={datosEmpleado&&datosEmpleado.phone}/>
+      <br />
+      <TextField disabled className={styles.inputMaterial} label="Compañia" name="company" onChange={handleChange} value={datosEmpleado&&datosEmpleado.company.name}/>
+      <br /><br />
+      <div align="right">
+        <Button color="primary" onClick={()=>peticionPut()}>Editar</Button>
+        <Button onClick={()=>abrirCerrarMoEditar()}>Cancelar</Button>
+      </div>
+    </div>
+  );
+
   return (
+    <Container>
+      <Alert color="warning" style={{margin:'10px auto', width:'700px'}}><strong>Warning:</strong> Entrara en una ventana donde se actualizan cambios</Alert>
+    
+    
     <div className="table-responsive" style={{width:'900px', margin:'60px auto', border:'2px solid black', borderRadius:'20px'}}>
       <MaterialTable
       columns={columnas}
@@ -48,7 +140,7 @@ function App() {
         {
           icon: 'edit',
           tooltip: 'Editar usuario',
-          onClick: (event, rowData)=>alert('Prueba donde deberia ir un modal con los datos de: '+rowData.name)
+          onClick: (event, rowData)=>usuarioSeleccionado(rowData, "Editar")
         }
       ]}
       options={{
@@ -60,7 +152,13 @@ function App() {
         }
       }}
       />
+      <Modal
+      open={moEditar}
+      onClose={abrirCerrarMoEditar}>
+        {bodyEditar}
+      </Modal>
     </div>
+    </Container>
   );
 } 
 
